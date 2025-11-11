f8e8b855abf04c925f68e25b1006796a130c2d62
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-11-11 00:19:39 -0300

    fix: docs

--- docs-fountain/docusaurus.config.ts
+++ docs-fountain/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs-fountain.vercel.app',
+  url: 'https://docs-fountain-xrpl.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
